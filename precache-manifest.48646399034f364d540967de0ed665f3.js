8a7dfe891b9019c7874fc444723f6cbc7e621a2b
ericodesu <erickfcaldeira@gmail.com>
2022-12-15 20:23:47 -0300

    Updates

---
+++ precache-manifest.48646399034f364d540967de0ed665f3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ac97f20e4a8ef0eaab7e012a157b6d",
+    "url": "/namah/index.html"
+  },
+  {
+    "revision": "8e1363262a5cb3050c36",
+    "url": "/namah/static/css/main.509416f1.chunk.css"
+  },
+  {
+    "revision": "ada780e9c3e863194748",
+    "url": "/namah/static/js/2.a4797f58.chunk.js"
+  },
+  {
+    "revision": "caf385b8edbdf01fe3dc4c5c3a8efb1f",
+    "url": "/namah/static/js/2.a4797f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1363262a5cb3050c36",
+    "url": "/namah/static/js/main.5fe8a066.chunk.js"
+  },
+  {
+    "revision": "9cac8f57bc3b73bb3a65",
+    "url": "/namah/static/js/runtime-main.18589023.js"
+  },
+  {
+    "revision": "1cf9c73216a3e6ce8354f8ae88756d20",
+    "url": "/namah/static/media/Adamina-Regular.1cf9c732.eot"
+  },
+  {
+    "revision": "32e31ea6c8f38bce55b5c377cb66d84d",
+    "url": "/namah/static/media/Adamina-Regular.32e31ea6.woff"
+  },
+  {
+    "revision": "702b0d7edc9f00c49d11c6c9753fc5d6",
+    "url": "/namah/static/media/Adamina-Regular.702b0d7e.svg"
+  },
+  {
+    "revision": "ce0435ddfd9fc5efcac92ad41ce93be8",
+    "url": "/namah/static/media/Adamina-Regular.ce0435dd.ttf"
+  },
+  {
+    "revision": "1b04f6bf96852188b6de37ba12b01ab1",
+    "url": "/namah/static/media/Chloe-Regular.1b04f6bf.svg"
+  },
+  {
+    "revision": "328d3e21183998a1cdda3372123f6d70",
+    "url": "/namah/static/media/Chloe-Regular.328d3e21.woff"
+  },
+  {
+    "revision": "3f75a25d28995302e73fb0490e79b35d",
+    "url": "/namah/static/media/Chloe-Regular.3f75a25d.eot"
+  },
+  {
+    "revision": "e505f205cc5a645580b2dbf27c1e3874",
+    "url": "/namah/static/media/Chloe-Regular.e505f205.ttf"
+  },
+  {
+    "revision": "0229f54a4e5cfdbd3cfdacbe86e99e0c",
+    "url": "/namah/static/media/ProximaNova-Bold.0229f54a.svg"
+  },
+  {
+    "revision": "21bae7f72b17ee2d81162136a1c9cdf4",
+    "url": "/namah/static/media/ProximaNova-Bold.21bae7f7.ttf"
+  },
+  {
+    "revision": "3b70f0d9979f78e3780fd77269c3fe9f",
+    "url": "/namah/static/media/ProximaNova-Bold.3b70f0d9.woff"
+  },
+  {
+    "revision": "caa7d8653555e3c3d30a5c860acea36d",
+    "url": "/namah/static/media/ProximaNova-Bold.caa7d865.eot"
+  },
+  {
+    "revision": "6d279ef230b5de952fbbb952ddfd98aa",
+    "url": "/namah/static/media/ProximaNova-Regular.6d279ef2.svg"
+  },
+  {
+    "revision": "7e564d19e8fbcf7170540582fc5a1c9f",
+    "url": "/namah/static/media/ProximaNova-Regular.7e564d19.ttf"
+  },
+  {
+    "revision": "e439d2f5096644b34258bd8be9f4ed8f",
+    "url": "/namah/static/media/ProximaNova-Regular.e439d2f5.woff"
+  },
+  {
+    "revision": "f3d966fb4879b9448d0828e6b16a1025",
+    "url": "/namah/static/media/ProximaNova-Regular.f3d966fb.eot"
+  },
+  {
+    "revision": "53b53a1a120d34a769aa59c5def113a0",
+    "url": "/namah/static/media/ProximaNova-Semibold.53b53a1a.eot"
+  },
+  {
+    "revision": "7c17940a9a40805f233609a2f0c78080",
+    "url": "/namah/static/media/ProximaNova-Semibold.7c17940a.svg"
+  },
+  {
+    "revision": "f83281a7796841966c252267f3b90827",
+    "url": "/namah/static/media/ProximaNova-Semibold.f83281a7.ttf"
+  },
+  {
+    "revision": "fc0c9d86159b38851d673ecd8e0077da",
+    "url": "/namah/static/media/ProximaNova-Semibold.fc0c9d86.woff"
+  },
+  {
+    "revision": "10aa429114b0ff879f138e3a174fd9eb",
+    "url": "/namah/static/media/ProximaNova-Thin.10aa4291.woff"
+  },
+  {
+    "revision": "221a41a17d66a91f3f73804ed8398c66",
+    "url": "/namah/static/media/ProximaNova-Thin.221a41a1.ttf"
+  },
+  {
+    "revision": "2aab54cc3d4d611bc4b30c824cbc491b",
+    "url": "/namah/static/media/ProximaNova-Thin.2aab54cc.eot"
+  },
+  {
+    "revision": "7bb3b456134071905355dc6895d99987",
+    "url": "/namah/static/media/ProximaNova-Thin.7bb3b456.svg"
+  },
+  {
+    "revision": "3f6357ec2fc4ff0d8da405ceac5ed006",
+    "url": "/namah/static/media/podcast-place-holder.3f6357ec.mp3"
+  }
+]);
